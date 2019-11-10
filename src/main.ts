996dfb43d9e323005eda8dbf259009a83dd0d822
Jan Sipr <sipr.jan@gmail.com>
2019-11-10 21:10:25 +0100

    Some more work

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { ErrorMapper } from "ErrorMapper";
+import { runRole } from "creeps";
 
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+    console.log(`Current game tick is ${Game.time}`);
 
-  // Automatically delete memory of missing creeps
-  for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
-      delete Memory.creeps[name];
+    // Automatically delete memory of missing creeps
+    for (const name in Memory.creeps) {
+        if (!(name in Game.creeps)) {
+            delete Memory.creeps[name];
+        }
     }
-  }
+
+    for (let creep in Game.creeps)
+    {
+        runRole(Game.creeps[creep]);
+    }
 });
